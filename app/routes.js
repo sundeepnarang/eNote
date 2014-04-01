ed84050f666c78bd8d0b964fc372e1e0206ba433
sundeepnarang <sundeepnrng@gmail.com>
2014-04-01 17:55:33 -0400

    signup fixed

--- app/routes.js
+++ app/routes.js
@@ -58,7 +58,7 @@ module.exports = function(app, passport,fs,lib) {
 
 		// process the signup form
 		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
+			successRedirect : '/home', // redirect to the secure profile section
 			failureRedirect : '/signup', // redirect back to the signup page if there is an error
 			failureFlash : true // allow flash messages
 		}));
