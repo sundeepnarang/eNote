449299f7b61e74b444f8f159ae248a98e7132216
YingchuChen <cyc870830@gmail.com>
2014-04-10 23:51:25 -0400

    for test

--- server.js
+++ server.js
@@ -26,22 +26,7 @@ mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
-app.configure(function() {
 
-	// set up our express application
-	app.use(express.logger('dev')); // log every request to the console
-	app.use(express.cookieParser()); // read cookies (needed for auth)
-	app.use(express.bodyParser()); // get information from html forms
-
-	app.set('view engine', 'ejs'); // set up ejs for templating
-
-	// required for passport
-	app.use(express.session({ store: sessionStore,secret: 'ilovescotchscotchy.scotchscotch' , key: 'express.sid'})); // session secret
-	app.use(passport.initialize());
-	app.use(passport.session()); // persistent login sessions
-	app.use(flash()); // use connect-flash for flash messages stored in session
-
-});
 
 
 // routes ======================================================================
